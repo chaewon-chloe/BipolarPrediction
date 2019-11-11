61ee0e8df056d747cb65e3c59cb2738349f38783
chaewon-chloe <57304377+chaewon-chloe@users.noreply.github.com>
2019-11-11 01:33:39 -0500

    Update VariableSelection.R

--- VariableSelection.R
+++ VariableSelection.R
@@ -1,1 +1,13 @@
+library(glmnet)
 
+# ds1
+(x = as.matrix(dat_new[,-1]))
+(y = dat_new$SuiHx)
+
+set.seed(111)
+cv.glmmod <- cv.glmnet(x, y=as.factor(y), alpha = 1, family="binomial", nfolds = 56, type.measure = "class")
+names(cv.glmmod)
+plot(cv.glmmod)
+(best.lambda <- cv.glmmod$lambda.min)
+cv.glmmod$lambda
+coef(cv.glmmod, s = 0.1168796)
